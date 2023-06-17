f3e61834f515e276850747e9676cdc22b447e1e9
Elaidzha1940 <eli080293@gmail.com>
2023-06-17 16:49:53 +0300

    Neumorphic

---
+++ neumorphic Items/ContentView.swift
@@ -0,0 +1,27 @@
+//  /*
+//
+//  Project: neumorphic Items
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.06.2023
+//
+//  Status
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ neumorphic Items/neumorphic_ItemsApp.swift
@@ -0,0 +1,21 @@
+//  /*
+//
+//  Project: neumorphic Items
+//  File: neumorphic_ItemsApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 17.06.2023
+//
+//  Status
+//
+//  */
+
+import SwiftUI
+
+@main
+struct neumorphic_ItemsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
